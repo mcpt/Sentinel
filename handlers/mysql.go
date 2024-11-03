4a9049bfa77578f6a9be6d6c3dbdbf549fce84ac
Jason Cameron <git@jasoncameron.dev>
2024-11-03 01:20:33 -0500

    upgrade mysqldump's speed & output fmt
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- handlers/mysql.go
+++ handlers/mysql.go
@@ -24,11 +24,14 @@ func (h *MySQLHandler) Backup(ctx context.Context) (string, error) {
 	timestamp := time.Now().Format("20060102_150405")
 	filename := filepath.Join(h.tempDir, fmt.Sprintf("mysql_%s.sql", timestamp))
 	cmd := exec.CommandContext(ctx, "mysqldump",
+		"--single-transaction",
 		"-h", config.Cfg.MySQL.Host,
 		"-P", config.Cfg.MySQL.Port,
 		"-u", config.Cfg.MySQL.User,
 		fmt.Sprintf("-p%s", config.Cfg.MySQL.Password),
-		config.Cfg.MySQL.Database)
+		config.Cfg.MySQL.Database,
+		"--quick",
+		"-r ", filename)
 
 	output, err := cmd.Output()
 	if err != nil {
