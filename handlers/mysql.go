3c2dbada2a171443f19ddeeb1819f6e294f7476a
Jason Cameron <git@jasoncameron.dev>
2024-11-03 21:21:58 -0500

    Have the mysqldump go to stdout (pipes)
    Signed-off-by: Jason Cameron <git@jasoncameron.dev>

--- handlers/mysql.go
+++ handlers/mysql.go
@@ -76,14 +76,13 @@ func (h *MySQLHandler) getTotalSize(ctx context.Context) (int64, error) {
 }
 
 // createMySQLDumpCommand creates the mysqldump command with proper parameters
-func (h *MySQLHandler) createMySQLDumpCommand(ctx context.Context, filename string) *exec.Cmd {
+func (h *MySQLHandler) createMySQLDumpCommand(ctx context.Context) *exec.Cmd {
 	return exec.CommandContext(ctx, "mysqldump", // #nosec  G204 -- All data here is coming from the config file,
 		// which if someone can modify, they can do anything they want
 		"--single-transaction",
 		"--extended-insert",
 		"--create-options",
 		"--quick",
-		"--result-file="+filename,
 		"-h", config.Cfg.MySQL.Host,
 		"-P", config.Cfg.MySQL.Port,
 		"-u", config.Cfg.MySQL.User,
@@ -122,7 +121,7 @@ func (h *MySQLHandler) Backup(ctx context.Context) (string, error) {
 	)
 
 	// Create and configure mysqldump command
-	cmd := h.createMySQLDumpCommand(ctx, filename)
+	cmd := h.createMySQLDumpCommand(ctx)
 
 	// Set up pipes for output handling
 	stdout, err := cmd.StdoutPipe()
